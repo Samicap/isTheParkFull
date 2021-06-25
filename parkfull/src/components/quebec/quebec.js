27b26ce38d1942034f8fa02bbcf6919d5d104610
samicap <lovestochasewindmills@gmail.com>
2021-06-25 17:54:31 -0400

    add buttons that log the time clicked in Quebec component

--- parkfull/src/components/quebec/quebec.js
+++ parkfull/src/components/quebec/quebec.js
@@ -1,22 +1,28 @@
-// import { styled } from '@material-ui/core/styles';
-// import Button from '@material-ui/core/Button';
+
+import { useState } from 'react';
 import './quebec.css';
 import { MyButton } from '../../styles/buttons';
+// Will eventually need to make a component that can be looped over and repeated here that holds the info on parks.
 
-// const MyButton = styled(Button)({
-//   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-//   border: 0,
-//   borderRadius: 3,
-//   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//   color: 'white',
-//   height: 48,
-//   padding: '0 30px',
-// });
+//need a function to display the time the full button was clicked
+//button clicked functions need to be in thier own folder to be universal and passed down to components
 
-export default function Quebec() {
+export default function Quebec() { 
+  const [ timeClickedFull, setTimeClickedFull ] = useState();
+  
+  function timeFullButtonClicked() {
+    let timeClicked = new Date().toString();
+    setTimeClickedFull(timeClicked)
+  }
+
   return (
-    <div>
-      <MyButton>Full</MyButton>
+    <div className="park-container">
+      Oka Beach National Park
+      <p>
+        {/* <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia-cdn.tripadvisor.com%2Fmedia%2Fphoto-s%2F0c%2F4b%2Fb3%2F24%2Foka-beach.jpg&imgrefurl=https%3A%2F%2Fwww.tripadvisor.com%2FAttraction_Review-g1575408-d10642386-Reviews-Oka_Beach-Oka_Quebec.html&tbnid=f5IXwWjkXWJc1M&vet=12ahUKEwja9enprbPxAhUEVt8KHZxcDiIQMygAegUIARCIAQ..i&docid=Ya3LVaAp2g3R3M&w=550&h=413&q=oka%20beach&ved=2ahUKEwja9enprbPxAhUEVt8KHZxcDiIQMygAegUIARCIAQ" alt="oka beach"></img> */}
+        <MyButton onClick={timeFullButtonClicked}>Full </MyButton>  {timeClickedFull} <MyButton>Open</MyButton>
+
+      </p>
     </div>
   )
 }
